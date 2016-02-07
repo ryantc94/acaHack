e01b1fc23e480e842fede92bddbad291872eae04
Edwin Wu <edwinwu2017@u.northwestern.edu>
2016-02-06 20:20:31 -0600

    Pass_data more basic with numeric values
    a is just data from Illinois

--- pass_data.py
+++ pass_data.py
@@ -5,33 +5,72 @@ import pymongo
 import json
 import urllib
 import datetime
+import ujson
 import pyexcel as pe
-import pyexcel.ext.xlsx 
+import pyexcel.ext.xlsx
 
 
 client = pymongo.MongoClient()
-
 today = datetime.date.today()
 today = today.strftime("%Y%m%d")
-client = pymongo.MongoClient()
-#creat database and collection
-db = client['ACAHack']
-col = 'formulary_'+ today
+# creat database and collection
+db1 = client['ACAHack']
+col = 'formulary_' + today
 
 
 #open file
-records = pe.get_records(file_name="machine-readable-url-puf.xlsx")
+records = pe.get_records(file_name="a.xlsx")
 counter = 0
 for record in records:
-	url = record["URL Submitted"]
-	if ".json" in url:
+    url = record["URL Submitted"]
+    if ".json" in url:
 
-		response = urllib.urlopen(url)
-		data = json.loads(response.read())
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
 
-		for url2 in data["formulary_urls"]:
-			response2 = urllib.urlopen(url2)
-			dic = json.loads(response2.read())
-			db[col].insert(dic)
-			counter+=1
-			print ("importing...",counter," to Mongo")
+        for url2 in data["formulary_urls"]:
+            print url2
+            response2 = urllib.urlopen(url2)
+            print type(response2)
+            dic = ujson.loads(response2.read())
+            for item in dic:
+                try:
+                    newdic = {"rxnorm_id": item["rxnorm_id"]}
+                    print item["rxnorm_id"]
+                    oneplan = False
+                    for plan in item["plans"]:
+                        ILLINOISCHECKER = plan["plan_id"]
+                        if "IL" in ILLINOISCHECKER:
+                            oneplan = True
+                            if plan["drug_tier"] == "PREFERRED-GENERIC":
+                                newdic[plan["plan_id"]] = 7
+
+                            elif plan["drug_tier"] == "GENERIC-BRAND":
+                                newdic[plan["plan_id"]]=6
+
+                            elif plan["drug_tier"] == "NON-PREFERRED-GENERIC":
+                                newdic[plan["plan_id"]] = 5
+
+                            elif plan["drug_tier"] == "PREFERRED-BRAND":
+                                newdic[plan["plan_id"]] = 4
+                            elif plan["drug_tier"] == "NON-PREFERRED-BRAND":
+                                newdic[plan["plan_id"]] = 3
+                            elif plan["drug_tier"] == "GENERIC-BRAND-SPECIALITY":
+                                newdic[plan["plan_id"]] = 2
+                            elif plan["drug_tier"] == "GENERIC-BRAND-SPECIALITY-DRUGS":
+                                newdic[plan["plan_id"]] = 2
+                            elif plan["drug_tier"] == "SPECIALITY-DRUGS":
+                                newdic[plan["plan_id"]] = 1
+                            elif plan["drug_tier"] == "SPECIALITY BRAND":
+                                newdic[plan["plan_id"]] = 1
+                            else:
+                                print plan["drug_tier"]
+                    if oneplan:
+                        db1[col].insert(newdic)
+                        print "inserted"
+
+                except:
+                    print "ITEM WITHOUT ID"
+                    continue
+            counter += 1
+            print ("importing...", counter, " to Mongo")
